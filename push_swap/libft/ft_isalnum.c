1673646748fedd32ef39a48e8c32c1c3da8cfd1c
bilal <bilalerrabia@gmail.com>
2025-09-30 22:26:37 +0000

    push

--- push_swap/libft/ft_isalnum.c
+++
@@ -1,5 +0,0 @@
-int ft_isalnum(char c)
-{
-    return (((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) 
-            || (c >= '0' && c <= '9'));
-}
--- push_swap/libft/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "libft.h"
-int main()
-{
-    char str[27];
-    ft_memset(str, 65, 2);
-    ft_putstr_fd(str, 1);
-}
